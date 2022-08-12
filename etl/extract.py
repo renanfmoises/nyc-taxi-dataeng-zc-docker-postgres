9011b0fee1124f9dbe94f35694332c9d93a23969
Renan Moises <renanfmoises@gmail.com>
2022-08-12 17:42:26 -0300

    Modularized ETL

--- etl/extract.py
+++ etl/extract.py
@@ -1,27 +1,27 @@
 """ This function downloads the parquet file and converts and stores it to csv. """
 
+from dataclasses import dataclass
 import os
 import pandas as pd
 
-
+@dataclass
 class Extract:
-    def __init__(
-        self, url, parquet_file_name="output.parquet", csv_file_name="output.csv"
-    ):
-        self.url = url
-        self.parquet_file_name = parquet_file_name
-        self.csv_file_name = csv_file_name
-        self.__df = None
+    url: str
+    parquet_file_name: str = "output.parquet"
+    csv_file_name: str = "output.csv"
+    df = None
+    n_rows = None
 
     def download_parquet(self):
+        """ Download the parquet file and converts and stores it to csv. """
         os.system(f"wget {self.url} -O {self.parquet_file_name}")
 
-    def get_df(self):
-        self.__df = pd.read_parquet(self.parquet_file_name)
-
-    def n_rows(self):
-        n_rows = self.__df.shape[0]
-        return n_rows
+    def get_pandas_df(self):
+        """ Load the pandas dataframe into memmory and update n_rows attribute. """
+        self.df = pd.read_parquet(self.parquet_file_name)
+        self.n_rows = self.df.shape[0]
+        return self.df, self.n_rows
 
     def convert_parquet_to_csv(self):
-        self.__df.to_csv(self.csv_file_name)
+        """ Convert the parquet file to csv. """
+        self.df.to_csv(self.csv_file_name, index=False, header=True)
